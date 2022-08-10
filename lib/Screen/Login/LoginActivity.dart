5748f2d02d65c8add8ba8421a6766e7517eacc91
TranBaoPhuc <phuc423vn@gmail.com>
2022-08-10 07:25:14 +0700

    update login

--- lib/Screen/Login/LoginActivity.dart
+++ lib/Screen/Login/LoginActivity.dart
@@ -62,8 +62,6 @@ class _LoginActivityState extends State<LoginActivity> {
 
   @override
   Widget build(BuildContext context) {
-    _usernameController.text = "waiter1";
-    _passwordController.text = "123456";
     return Scaffold(
       body: Stack(
         children: <Widget>[
